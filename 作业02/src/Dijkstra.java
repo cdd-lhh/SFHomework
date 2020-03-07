b0037d819cd4bb285fc08736ac2f31a1df61f00f
cdd-lhh <1978435821@qq.com>
2020-03-07 23:04:20 +0800

    作业02

--- 作业02/src/Dijkstra.java
+++ 作业02/src/Dijkstra.java
@@ -1,37 +1,34 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Dijkstra {
 
     static int m = Integer.MAX_VALUE;
 
     static class Entity {
         int[][] matrix;
-        List nodes;
+        char[] nodes;
         boolean[] visited = new boolean[100005];
         int dis[] = new int[10005];
 
-        public Entity(List nodes, int[][] matrix) {
+        public Entity(char[] nodes, int[][] matrix) {
             this.nodes = nodes;
             this.matrix = matrix;
         }
 
         public void Impl() {
 //            //初始化点与点之间的初始距离
-            int[][] distance = new int[nodes.size()][nodes.size()];
-            for (int i = 0; i < nodes.size(); i++) {
-                for (int j = 0; j < nodes.size(); j++) {
+            int[][] distance = new int[nodes.length][nodes.length];
+            for (int i = 0; i < nodes.length; i++) {
+                for (int j = 0; j < nodes.length; j++) {
                     distance[i][j] = matrix[i][j];
                 }
             }
-            for (int i = 0; i < nodes.size(); i++) {
+            for (int i = 0; i < nodes.length; i++) {
                 dis[i] = distance[0][i];
             }
             visited[0] = true;
-            for (int i = 0; i < nodes.size(); i++) {
+            for (int i = 0; i < nodes.length; i++) {
                 int min = m;
                 int index = -1;
-                for(int j = 0; j < nodes.size(); j++){
+                for(int j = 0; j < nodes.length; j++){
                     if(visited[j] == false){
                         if(dis[j] < min){
                             index = j;
@@ -42,7 +39,7 @@ public class Dijkstra {
                 if(index != -1)
                     visited[index] = true;
                 //更新
-                for(int k = 0; k < nodes.size(); k++){
+                for(int k = 0; k < nodes.length; k++){
                     if(visited[k] == false){
                         if(distance[index][k] != m  && (min + distance[index][k] < dis[k])){
                             dis[k] = min + distance[index][k];
@@ -50,23 +47,15 @@ public class Dijkstra {
                     }
                 }
             }
-            for(int i = 0;i < nodes.size(); i++){
-                System.out.println("a到"+ nodes.get(i)+ "的距离是：" +dis[i]);
+            for(int i = 0;i < nodes.length; i++){
+                System.out.println("a到"+ nodes[i]+ "的距离是：" +dis[i]);
             }
         }
     }
         public static void main(String[] args) {
-            List nodes = new ArrayList();
-            nodes.add('a');
-            nodes.add('b');
-            nodes.add('c');
-            nodes.add('d');
-            nodes.add('e');
-            nodes.add('f');
-            nodes.add('g');
-            nodes.add('h');
+        char nodes[] = {'a', 'b', 'c', 'd', 'e', 'f','g','h'};
             int matrix[][] = new int[][]{
-                    /*a b  c  d  e  f  g  h*/
+                         /*a b  c  d  e  f  g  h*/
                     /*a*/{0, 1, m, m, m, m, m, m},
                     /*b*/{m, 0, m, 2, m, m, m, m},
                     /*c*/{2, m, 0, m, m, m, m, m},
